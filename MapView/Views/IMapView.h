6953b541439c509535cf202acece7c9d8b7e64c8
流年释残忆 <291753906@qq.com>
2017-06-19 21:08:48 +0800

    v

---
+++ MapView/Views/IMapView.h
@@ -0,0 +1,13 @@
+//
+//  IMapView.h
+//  MapView
+//
+//  Created by liyiping on 2017/6/19.
+//  Copyright © 2017年 情风. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol IMapView <NSObject>
+
+@end
